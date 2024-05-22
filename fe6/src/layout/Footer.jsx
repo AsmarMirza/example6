05da6f24b848ac6f1285e40be0df3ed1b8cdb5fe
unknown <asmarmirzayeva66@gmail.com>
2024-05-22 08:40:28 +0400

    modiifed

--- fe6/src/layout/Footer.jsx
+++ fe6/src/layout/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import "./footer.scss"
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+<div className='footerBoxes'>
+  <div className='footerFirst'>
+<div>
+<img src="https://preview.colorlib.com/theme/course/images/logo.png.webp" alt="" />
+        <h1> COURSE</h1>
+</div>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum, tempor lacus.</p>
+  </div>
+
+  <div className='footerSecond'>
+<h1>Menu</h1>
+<div className='footerList'>
+<p>Home</p>
+<p>About Us</p>
+<p>Courses</p>
+<p>News</p>
+<p>Contact</p>
+</div>
+
+  </div>
+
+</div>
+    </div>
   )
 }
 
--- fe6/src/pages/Home.jsx
+++ fe6/src/pages/Home.jsx
@@ -52,13 +52,144 @@ function Home() {
       </section>
 
       <section className='main'>
+<div className='left'>
+<h1 className='mainDif'>Register now and get a discount <span className='mainDiff'>50% </span> discount until 1 January</h1>
+<p className='mainText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempo.</p>
+<button className='rightBtn'>Register Now</button>
+</div>
 <div className='right'>
-<h1>Register now and get a discount <span>50% </span> discount until 1 January</h1>
-<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempo.</p>
-<button className='rightBtn'>Registor Now</button>
+  <h1 className='mainDif'>Search for your course</h1>
+  <div className='mainInput'>
+
+  <input className='mainInp' type="text" placeholder='Course Name' />
+  <input className='mainInp' type="text" name="" id="" placeholder='Category' />
+  <input className='mainInp' type="text" name="" id="" placeholder='Degree' />
+  <button className='inpBtn'>Search Course</button>
+  </div>
 </div>
 
       </section>
+
+      <section className='services'>
+        
+
+       
+<h1 className='servicesHeading'>Our Services</h1>
+
+<div className='serviceBoxes'>
+  <div className='serviceBox'>
+    <div className='sourceImg'>
+      <img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/earth-globe.svg" alt="" />
+    </div>
+<h3 className='serviceHeading'>Online Courses</h3>
+<p className='serviceText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+  </div>
+
+
+  <div className='serviceBox'>
+    <div className='sourceImg'>
+      <img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/exam.svg" alt="" />
+    </div>
+<h3 className='serviceHeading'>Indoor Courses</h3>
+<p className='serviceText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+  </div>
+
+
+  <div className='serviceBox'>
+    <div className='sourceImg'>
+      <img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/books.svg" alt="" />
+    </div>
+<h3 className='serviceHeading'>Amazing Library</h3>
+<p className='serviceText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+  </div>
+
+
+  <div className='serviceBox'>
+    <div className='sourceImg'>
+      <img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/professor.svg" alt="" />
+    </div>
+<h3 className='serviceHeading'>Exceptional Professors</h3>
+<p className='serviceText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+  </div>
+
+
+  <div className='serviceBox'>
+    <div className='sourceImg'>
+      <img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/blackboard.svg" alt="" />
+    </div>
+<h3 className='serviceHeading'>Top Programs</h3>
+<p className='serviceText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+  </div>
+
+
+  <div className='serviceBox'>
+    <div className='sourceImg'>
+      <img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/mortarboard.svg" alt="" />
+    </div>
+<h3 className='serviceHeading'>Graduate Diploma</h3>
+<p className='serviceText'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+  </div>
+
+</div>
+
+      </section>
+
+<section className='events'>
+<div className='eventsMain'>
+<h1 className='eventsHeading'>Upcoming Events</h1>
+<div className='events_'>
+
+  <div  className='eventsLeft'>
+<h3 className='eventsBoxHead'>07</h3>
+<p className='eventsBoxText'>January</p>
+  </div>
+  <div className='eventsMiddle'>
+<h3 className='middleHead'>Student Festival</h3>
+<p className='middleText1'>Grand Central Park</p>
+<p className='middleText2'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+  </div>
+  <div className='eventsRight'>
+<img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/event_1.jpg.webp" alt="" />
+  </div>
+</div>
+
+
+<div className='events_'>
+
+  <div  className='eventsLeft'>
+<h3 className='eventsBoxHead'>07</h3>
+<p className='eventsBoxText'>January</p>
+  </div>
+  <div className='eventsMiddle'>
+<h3 className='middleHead'>Open day in the Univesrsity campus</h3>
+<p className='middleText1'>Grand Central Park</p>
+<p className='middleText2'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+  </div>
+  <div className='eventsRight'>
+<img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/event_2.jpg.webp" alt="" />
+  </div>
+</div>
+
+
+
+<div className='events_'>
+
+  <div  className='eventsLeft'>
+<h3 className='eventsBoxHead'>07</h3>
+<p className='eventsBoxText'>January</p>
+  </div>
+  <div className='eventsMiddle'>
+<h3 className='middleHead'>Student Graduation Ceremony</h3>
+<p className='middleText1'>Grand Central Park</p>
+<p className='middleText2'>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+  </div>
+  <div className='eventsRight'>
+<img style={{width:"100%"}} src="https://preview.colorlib.com/theme/course/images/event_3.jpg.webp" alt="" />
+  </div>
+</div>
+
+</div>
+</section>
 
 
 
